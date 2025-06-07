60fe719da66b32a7d7a4774b4b1760df09ea9269
nazaninJh <nazanin.jh97@gmail.com>
2025-06-07 14:19:19 +0330

    Add map component

--- src/components/map/map.component.tsx
+++ src/components/map/map.component.tsx
@@ -1,39 +1,92 @@
+"use client";
 import { isMobile } from "react-device-detect";
 
 import { AfricaImage } from "./assets/images/africa.image";
 import { IranImage } from "./assets/images/iran.image";
-import { ArrowIcon } from "./assets/icons/arrow.icon";
-
-import { Carousel } from "antd";
 
 import styles from "./map.module.scss";
-import { NextArrowIcon } from "./assets/icons/nextArrow";
+
+import { Carousel } from "antd";
+import { useState } from "react";
+import texts from "./assets/texts/map.json";
+import clsx from "clsx";
+import TextIcon from "./assets/icons/text.icon";
 
 export default function MapComponent() {
-  // note: make the svg an image so it can adjust it's size
+  const [currentContinent, setCurrentContinent] = useState<{
+    code: "africa" | "middle";
+    name: string;
+  }>({
+    code: "africa",
+    name: texts.title.africa,
+  });
+
+  const isAfrica = currentContinent.code === "africa";
+
+  const handleContinent = (slide: number) => {
+    if (slide === 0) {
+      setCurrentContinent({
+        code: "africa",
+        name: texts.title.africa,
+      });
+    } else {
+      setCurrentContinent({
+        code: "middle",
+        name: texts.title["middle-east"],
+      });
+    }
+  };
   return (
     <div className="wrapper">
       <div className={styles.container}>
         <div className={styles.item}>
           <Carousel
-            arrows
             dots={false}
-            prevArrow={<ArrowIcon />}
-            nextArrow={<NextArrowIcon />}
-            style={{
-              outline: "none",
-              border: "none",
-            }}
+            arrows={true}
+            afterChange={(slide) => handleContinent(slide)}
           >
             <AfricaImage
               width={isMobile ? 200 : 300}
               height={isMobile ? 239 : 507}
+              pointerEvents="none"
             />
             <IranImage
               width={isMobile ? 200 : 300}
               height={isMobile ? 239 : 507}
+              pointerEvents="none"
             />
           </Carousel>
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.title}>
+            <span>{texts.title.pt1} </span>
+            <div className={clsx(styles.continent, isAfrica && styles.africa)}>
+              <span
+                className={clsx(
+                  styles.text,
+                  isAfrica && styles.active,
+                  isAfrica && styles.africa
+                )}
+              >
+                {texts.title.africa}
+              </span>
+              <span
+                className={clsx(
+                  styles.text,
+                  !isAfrica && styles.active,
+                  !isAfrica && styles.east
+                )}
+              >
+                {texts.title["middle-east"]}
+              </span>
+              <span>
+                <TextIcon />
+              </span>
+            </div>
+
+            <span> {texts.title.pt2}</span>
+          </div>
+          <p className={styles.content}>{texts.content}</p>
         </div>
       </div>
     </div>
