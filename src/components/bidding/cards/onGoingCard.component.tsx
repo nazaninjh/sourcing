92db55a1f9450eaf0608a33855955d1899248241
nazaninJh <nazanin.jh97@gmail.com>
2025-06-07 16:08:31 +0330

    Add grid image component

--- src/components/bidding/cards/onGoingCard.component.tsx
+++ src/components/bidding/cards/onGoingCard.component.tsx
@@ -1,7 +1,7 @@
-import Image, { StaticImageData } from "next/image";
-
 import styles from "./onGoingCard.module.scss";
 import { ReactNode } from "react";
+import GridImageComponent from "@/components/shared/grid-image/gridImage.component";
+import { StaticImageData } from "next/image";
 export default function OnGoingCardComponent({
   imgSrc,
   topStartTxts,
@@ -29,42 +29,60 @@ export default function OnGoingCardComponent({
 }) {
   return (
     <div className={styles.wrapper}>
-      <Image src={imgSrc} alt="عکس پروژه" width={387} height={255} />
-      <div className={styles.overlay} />
+      <GridImageComponent
+        props={{
+          hasOverlay: true,
+          imgSrc,
+          topChildren: {
+            direction: "start",
+            child: (
+              <div className={styles["texts-top-container"]}>
+                <div className={styles["name-container"]}>
+                  <p className={styles.name}>{topStartTxts.name}</p>
+                  <div className={styles.divider} />
+                  <p className={styles.comp}>
+                    {topStartTxts.competitionCategory}
+                  </p>
+                </div>
 
-      <div className={styles["texts-top-container"]}>
-        <div className={styles["name-container"]}>
-          <p className={styles.name}>{topStartTxts.name}</p>
-          <div className={styles.divider} />
-          <p className={styles.comp}>{topStartTxts.competitionCategory}</p>
-        </div>
-
-        <div className={styles.category}>
-          <span>{topStartTxts.categoryName}</span>
-          <span> : </span>
-          <span>{topStartTxts.category}</span>
-        </div>
-      </div>
-      <div className={styles["texts-bottom-container"]}>
-        <div className={styles.participant}>
-          <span className={styles.icon}>{bottomTxts.icon.participant}</span>
-          <div className={styles.texts}>
-            <span>{bottomTxts.participantName}</span>
-            <span>
-              {bottomTxts.participantNum} {bottomTxts.participantUnit}
-            </span>
-          </div>
-        </div>
-        <div className={styles.price}>
-          <span className={styles.icon}>{bottomTxts.icon.price}</span>
-          <div className={styles.texts}>
-            <span>{bottomTxts.priceName}</span>
-            <span>
-              {bottomTxts.priceNum} {bottomTxts.priceUnit}
-            </span>
-          </div>
-        </div>
-      </div>
+                <div className={styles.category}>
+                  <span>{topStartTxts.categoryName}</span>
+                  <span> : </span>
+                  <span>{topStartTxts.category}</span>
+                </div>
+              </div>
+            ),
+          },
+          bottomChildren: {
+            direction: "start",
+            width: "100%",
+            child: (
+              <div className={styles["texts-bottom-container"]}>
+                <div className={styles.participant}>
+                  <span className={styles.icon}>
+                    {bottomTxts.icon.participant}
+                  </span>
+                  <div className={styles.texts}>
+                    <span>{bottomTxts.participantName}</span>
+                    <span>
+                      {bottomTxts.participantNum} {bottomTxts.participantUnit}
+                    </span>
+                  </div>
+                </div>
+                <div className={styles.price}>
+                  <span className={styles.icon}>{bottomTxts.icon.price}</span>
+                  <div className={styles.texts}>
+                    <span>{bottomTxts.priceName}</span>
+                    <span>
+                      {bottomTxts.priceNum} {bottomTxts.priceUnit}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ),
+          },
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/shared/grid-image/gridImage.component.tsx
@@ -0,0 +1,58 @@
+import Image, { StaticImageData } from "next/image";
+import styles from "./gridImage.module.scss";
+import { ReactNode } from "react";
+type IProps = {
+  imgSrc: StaticImageData;
+  hasOverlay: boolean;
+  topChildren?: {
+    direction: "end" | "start" | "center";
+    child: ReactNode;
+    width?: string;
+  };
+  bottomChildren?: {
+    direction: "end" | "start" | "center";
+    child: ReactNode;
+    width?: string;
+  };
+  centerChildren?: {
+    direction: "end" | "start" | "center";
+    child: ReactNode;
+    width?: string;
+  };
+};
+export default function GridImageComponent({ props }: { props: IProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <Image src={props.imgSrc} alt="عکس پروژه" width={387} height={255} />
+      {props.hasOverlay && <div className={styles.overlay} />}
+
+      <div
+        className={styles["top-children-container"]}
+        style={{
+          justifySelf: props.topChildren?.direction ?? "",
+          width: props.topChildren?.width ?? "auto",
+        }}
+      >
+        {props.topChildren?.child ?? null}
+      </div>
+      <div
+        className={styles["bottom-children-container"]}
+        style={{
+          justifySelf: props.bottomChildren?.direction ?? "",
+          width: props.bottomChildren?.width ?? "auto",
+        }}
+      >
+        {props.bottomChildren?.child ?? null}
+      </div>
+      <div
+        className={styles["center-children-container"]}
+        style={{
+          justifySelf: props.centerChildren?.direction ?? "",
+          width: props.centerChildren?.width ?? "auto",
+        }}
+      >
+        {props.centerChildren?.child ?? null}
+      </div>
+    </div>
+  );
+}
