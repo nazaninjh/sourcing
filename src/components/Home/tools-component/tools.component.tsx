868239f454c0a88c1e61104bdc29f226e80c3326
nazaninJh <nazanin.jh97@gmail.com>
2025-06-08 14:49:20 +0330

    Fix tools component style

--- src/components/Home/tools-component/tools.component.tsx
+++ src/components/Home/tools-component/tools.component.tsx
@@ -3,25 +3,26 @@ import texts from "./assets/texts/tools.json";
 import crane from "./assets/images/crane-pic.webp";
 import rectangle from "./assets/images/crane-rectangle.webp";
 import styles from "./tools.module.scss";
+import clsx from "clsx";
 
 export default function ToolsComponent() {
   return (
-    <section className={styles.container}>
-      <div className={styles.box}>
-        <div className={styles.bg} />
-        <div className={styles["bg-desktop"]}>
-          <Image src={rectangle} alt="جرثقیل" width={586} height={333} />
+    <div className={clsx(styles.wrapper, "wrapper")}>
+      <section className={styles.container}>
+        <div className={styles.box}>
+          <div className={styles.bg} />
+          <div className={styles["bg-desktop"]}>
+            <Image src={rectangle} alt="جرثقیل" fill />
+          </div>
+          <Image src={crane} alt="جرثقیل" width={650} height={470} />
         </div>
-        <Image src={crane} alt="جرثقیل" width={428} height={269} />
-      </div>
 
-      <div className="wrapper">
         <div className={styles.txt}>
           <p className={styles.title}>{texts.title}</p>
           <p className={styles.content}>{texts.content}</p>
           <button type="button">{texts.btnTxt}</button>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
